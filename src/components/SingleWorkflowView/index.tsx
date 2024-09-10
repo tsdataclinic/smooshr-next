3be051ae2f508290d7569511d7389cbc5f07d8aa
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2024-09-10 14:02:58 -0400

    Added floating action button to add new steps

--- src/components/SingleWorkflowView/index.tsx
+++ src/components/SingleWorkflowView/index.tsx
@@ -3,8 +3,16 @@ import { useParams } from 'wouter';
 import { WorkflowUtil } from '../../util/WorkflowUtil';
 import { processAPIData } from '../../util/apiUtil';
 import { WorkflowsService } from '../../client';
-import { Loader, Title, Group, Button } from '@mantine/core';
-import { IconDots } from '@tabler/icons-react';
+import {
+  Loader,
+  Title,
+  Group,
+  Button,
+  ActionIcon,
+  Affix,
+  Menu,
+} from '@mantine/core';
+import { IconDots, IconPlus } from '@tabler/icons-react';
 
 export function SingleWorkflowView(): JSX.Element {
   const params = useParams<{ workflowId: string }>();
@@ -38,6 +46,19 @@ export function SingleWorkflowView(): JSX.Element {
           </Group>
 
           <p>This page is still a work in progress.</p>
+          <Affix position={{ bottom: 40, right: 40 }}>
+            <ActionIcon color="blue" radius="xl" size={60}>
+              <Menu withArrow shadow="md" width={150} position="left">
+                <Menu.Target>
+                  <IconPlus stroke={1.5} size={30} />
+                </Menu.Target>
+                <Menu.Dropdown>
+                  <Menu.Item>Data Validation</Menu.Item>
+                  <Menu.Item>Flow Control</Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            </ActionIcon>
+          </Affix>
         </>
       ) : null}
     </>
