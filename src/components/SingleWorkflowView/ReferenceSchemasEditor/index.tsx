d20e843d88a3d31e8a5c90ce49513a89c1324579
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2024-09-11 14:47:31 -0400

    Added CSV upload button

--- src/components/SingleWorkflowView/ReferenceSchemasEditor/index.tsx
+++ src/components/SingleWorkflowView/ReferenceSchemasEditor/index.tsx
@@ -1,9 +1,20 @@
-import { Button } from '@mantine/core';
+import * as React from 'react';
+import { FileButton, Button, Text } from '@mantine/core';
 
 export function ReferenceSchemasEditor(): JSX.Element {
+  const [file, setFile] = React.useState<File | null>(null);
+
   return (
     <div>
-      <Button>Add CSV</Button>
+      <FileButton onChange={setFile} accept=".csv">
+        {(props) => <Button {...props}>Add CSV</Button>}
+      </FileButton>
+
+      {file ? (
+        <Text size="sm" ta="center" mt="sm">
+          Picked file: {file.name}
+        </Text>
+      ) : null}
     </div>
   );
 }
