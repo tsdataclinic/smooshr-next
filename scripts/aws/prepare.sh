843306a73c491403708994253fa6a80bc30ff354
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2024-09-27 12:34:24 -0400

    Loading nvm in aws scripts

--- scripts/aws/prepare.sh
+++ scripts/aws/prepare.sh
@@ -5,6 +5,7 @@
 export NVM_DIR="$HOME/.nvm"
 # This loads nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use 20
 
 APP_NAME=ek12
 cd /srv/$APP_NAME
--- scripts/aws/stop.sh
+++ scripts/aws/stop.sh
@@ -5,6 +5,7 @@
 export NVM_DIR="$HOME/.nvm"
 # This loads nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use 20
 
 # The `|| :` suppresses the exit code of 1 in case there are no
 # services running and forces pm2 to return an exit code 0
