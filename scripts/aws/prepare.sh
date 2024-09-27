ec1bf11eaaac5e14b7778a9451bf6c8420f0f336
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2024-09-27 14:19:44 -0400

    Added commands to load pyenv into the aws shell

--- scripts/aws/prepare.sh
+++ scripts/aws/prepare.sh
@@ -3,9 +3,15 @@
 
 # Load NVM
 export NVM_DIR="$HOME/.nvm"
-# This loads nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm use 20
+
+# Load pyenv into the shell
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+pyenv global 3.12
 
 APP_NAME=ek12
 cd /srv/$APP_NAME
--- scripts/aws/start.sh
+++ scripts/aws/start.sh
@@ -3,7 +3,6 @@
 
 # Load NVM
 export NVM_DIR="$HOME/.nvm"
-# This loads nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm use 20
 
