7f41142ea9d5becf1de2ad2d5a75eddf38826be0
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2024-10-02 11:49:48 -0500

    Removed a bunch of modals and moved them to a Workspace UI

--- src/components/SingleWorkflowView/index.tsx
+++ src/components/SingleWorkflowView/index.tsx
@@ -1,43 +1,15 @@
-import * as React from 'react';
-import { match } from 'ts-pattern';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'wouter';
 import { WorkflowUtil } from '../../util/WorkflowUtil';
 import { processAPIData } from '../../util/apiUtil';
 import { WorkflowsService } from '../../client';
-import {
-  Loader,
-  Title,
-  Group,
-  Button,
-  ActionIcon,
-  Stack,
-  Affix,
-  Menu,
-  Drawer,
-  Modal,
-  List,
-  Text,
-  Space,
-} from '@mantine/core';
-import { IconDots, IconPlus } from '@tabler/icons-react';
+import { Loader, Title, Group, Button, Menu, Modal } from '@mantine/core';
+import { IconDots } from '@tabler/icons-react';
 import { useDisclosure } from '@mantine/hooks';
-import { FieldsetSchemasEditor } from './FieldsetSchemasEditor';
-import { OperationEditor } from './OperationEditor';
 import { TestWorkflowBlock } from './TestWorkflowBlock';
 import { Workspace } from './Workspace';
 
-type ModalViewType = 'fieldsetSchemaEditor' | 'testWorkflow' | 'none';
-
-function getModalTitle(modalView: ModalViewType): string {
-  return match(modalView)
-    .with('fieldsetSchemaEditor', () => 'Configuring column schemas')
-    .with('testWorkflow', () => 'Test workflow')
-    .with('none', () => '')
-    .exhaustive();
-}
-
-export function SingleWorkflowView(): JSX.Element {
+export function SingleWorkflowView(): JSX.Element | null {
   const params = useParams<{ workflowId: string }>();
   const { data: workflow, isLoading } = useQuery({
     queryKey: WorkflowUtil.QUERY_KEYS.workflow(params.workflowId),
@@ -51,10 +23,8 @@ export function SingleWorkflowView(): JSX.Element {
       ),
   });
 
-  const [isBottomDrawerOpen, bottomDrawerActions] = useDisclosure(false);
-  const [isModalOpen, modalActions] = useDisclosure(false);
-  const [modalView, setModalView] = React.useState<ModalViewType>('none');
-  const fieldsetSchemas = workflow?.schema?.fieldsetSchemas ?? [];
+  const [isTestWorkflowModalOpen, testWorkflowModalActions] =
+    useDisclosure(false);
 
   console.log('Loaded workflow', workflow);
 
@@ -73,22 +43,9 @@ export function SingleWorkflowView(): JSX.Element {
             </Button>
           </Menu.Target>
           <Menu.Dropdown>
-            <Menu.Item
-              onClick={() => {
-                setModalView('fieldsetSchemaEditor');
-                modalActions.open();
-              }}
-            >
-              Add column schemas
-            </Menu.Item>
-            <Menu.Item disabled>Edit inputs</Menu.Item>
+            <Menu.Item disabled>Edit workflow</Menu.Item>
             <Menu.Item disabled>Publish workflow</Menu.Item>
-            <Menu.Item
-              onClick={() => {
-                setModalView('testWorkflow');
-                modalActions.open();
-              }}
-            >
+            <Menu.Item onClick={testWorkflowModalActions.open}>
               Test workflow
             </Menu.Item>
           </Menu.Dropdown>
@@ -101,60 +58,17 @@ export function SingleWorkflowView(): JSX.Element {
         {headerRow}
         <Workspace workflow={workflow} />
 
-        <Affix position={{ bottom: 40, left: 40 }}>
-          <Menu withArrow shadow="md" width={250} position="left">
-            <Menu.Target>
-              <ActionIcon color="blue" radius="xl" size={60}>
-                <IconPlus stroke={1.5} size={30} />
-              </ActionIcon>
-            </Menu.Target>
-            <Menu.Dropdown>
-              <Menu.Item onClick={bottomDrawerActions.open}>
-                Validate column schemas
-              </Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
-        </Affix>
-
         <Modal
-          opened={isModalOpen}
-          onClose={modalActions.close}
-          title={getModalTitle(modalView)}
+          opened={isTestWorkflowModalOpen}
+          onClose={testWorkflowModalActions.close}
+          title="Test workflow"
           size="auto"
         >
-          {match(modalView)
-            .with('fieldsetSchemaEditor', () => {
-              return (
-                <FieldsetSchemasEditor
-                  workflow={workflow}
-                  defaultFieldsetSchemas={fieldsetSchemas}
-                />
-              );
-            })
-            .with('testWorkflow', () => {
-              return <TestWorkflowBlock workflow={workflow} />;
-            })
-            .with('none', () => null)
-            .exhaustive()}
+          <TestWorkflowBlock workflow={workflow} />;
         </Modal>
-
-        <Drawer
-          offset={8}
-          radius="md"
-          opened={isBottomDrawerOpen}
-          onClose={bottomDrawerActions.close}
-          title="Configuring validation step: checking column schemas"
-          withOverlay={false}
-          position="bottom"
-        >
-          <OperationEditor
-            operationType="fieldsetSchemaValidation"
-            workflow={workflow}
-          />
-        </Drawer>
       </>
     );
   }
 
-  return <React.Fragment />;
+  return null;
 }
